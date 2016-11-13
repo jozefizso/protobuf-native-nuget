1809883985ec4c606cdbdf93d353868a6094ce09
Jozef Izso <jozef.izso@gmail.com>
2016-11-13 21:51:00 +0100

    Compile project on AppVeyor

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,12 @@
-@echo off
-setlocal
+@setlocal
+@setlocal EnableDelayedExpansion
 
-nuget pack protobuf-native.nuspec -OutputDirectory artifacts
+set _build=1
+@IF NOT "%APPVEYOR_BUILD_NUMBER%"=="" (
+  set _build=%APPVEYOR_BUILD_NUMBER%
+)
+
+set _version=3.0.0-preview%_build%
+
+nuget pack protobuf-native.nuspec -OutputDirectory artifacts -Version %_version% -NonInteractive
 exit /b %ERRORLEVEL%
